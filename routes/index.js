073eb7263c1a2016a9139157673bb25215e93afc
User <zipper@jacobzipper.com>
2017-10-14 09:22:09 -0400

    subStocks beta

--- routes/index.js
+++ routes/index.js
@@ -47,4 +47,27 @@ router.post('/addStocks', function(req, res, next) {
     });
 });
 
+router.post('/subStocks', function(req, res, next) {
+  db.one("SELECT portfolio FROM portfolios WHERE userid=${user};",{user:req.body.userId})
+      .then(user => {
+        console.log(user);
+        if(user.length!=0) {
+            console.log(user["portfolio"]);
+            var port = user["portfolio"];
+            console.log(port);
+            var stocks = Object.keys(port);
+            if(stocks.indexOf(req.body.ticker)!=-1) {
+                port[req.body.ticker]-=req.body.num;
+                port[req.body.ticker] = port[req.body.ticker] > 0 ? port[req.body.ticker] : 0;
+            }
+            db.none("UPDATE portfolios SET portfolio=${portf} WHERE userid=${user};",{user:req.body.userId,portf:port});
+        }
+        res.json({error:1});
+    })
+      .catch(error => {
+        console.log(error);
+        res.json({error:2});
+    });
+});
+
 module.exports = router;
